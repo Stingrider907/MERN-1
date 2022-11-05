aae62a297abf87473781dc3805426da855565d56
Stingrider907 <907.travis.henry@gmail.com>
2022-11-05 16:46:23 -0600

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-const http = require("http");
-
-http.createServer(function(request, response) {    
-   response.writeHead(200, {"Content-Type": "text/html"});
-   response.write("<h1>Hello, Node.js!</h1>");
-   response.write("<h2>This is Travis Brase -m63r576</h2><br><p>running on port 3030</p>"); 
-   response.end();
-}).listen(3030);
